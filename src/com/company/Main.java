f7cb0370bd9cded3db232b52010411f7aa6e1c41
Pavel <impetus@yandex.ru>
2021-01-27 17:24:35 +0300

    Синхронизация поток

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,46 @@
 package com.company;
 public class Main {
     // [+][-]
-    public static void main(String[] args) {
-	    new MyThread("+").start();
-        new MyThread("-").start();
+    public static void main(String[] args) throws InterruptedException {
+
+	    MyThread thread1 = new MyThread("+");
+	    thread1.start();
+        MyThread thread2 = new MyThread("-");
+        thread2.start();
+        Thread.sleep(2000);
+        thread1.flag = false;
+        thread1.join(); // ждем завершения 1-потока
+        test("Stopped 1-st thread!");
     }
+
+    public static Object key = new Object();
+
     public static void test(String message) {
-        try {
-            System.out.print("[");
-            Thread.sleep(1000);
-            System.out.print(message);
-            Thread.sleep(1000);
-            System.out.print("]");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        synchronized (key) {
+            try {
+                System.out.print("[");
+                Thread.sleep(1000);
+                System.out.print(message);
+                Thread.sleep(1000);
+                System.out.print("]");
+             //   key.notify(); // возобновить поток
+              //  key.wait(); // поставить поток в режим ожидания
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
 class MyThread extends Thread {
     private String mess;
+    public boolean flag = true;
+
     MyThread(String m) {
         this.mess = m;
     }
     @Override
     public void run() {
-        while(true) {
+        while(flag) {
             Main.test(mess);
         }
     }
